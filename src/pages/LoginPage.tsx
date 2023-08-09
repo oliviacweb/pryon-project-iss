49304e2623cdc6cb7bf62ab08a91c4eeeba5d31a
Olivia Webster <oliviacweb@gmail.com>
2023-08-08 23:17:20 -0600

    Add Facebook Login

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,8 +3,8 @@ import { useSelector, useDispatch } from 'react-redux';
 import { login } from '../redux/slices/authSlice';
 import { RootState } from '../redux/store';
 import { Navigate } from "react-router-dom";
-import { signInWithGoogle, signInWithEmail } from '../services/auth';
-import { RiGoogleFill } from 'react-icons/ri';
+import { signInWithGoogle, signInWithFacebook, signInWithEmail } from '../services/auth';
+import { RiFacebookFill, RiGoogleFill } from 'react-icons/ri';
 import { toast } from "react-toastify";
 import styles from '../styles/pages/LoginPage.module.scss';
 
@@ -36,6 +36,17 @@ const LoginPage: React.FC = () => {
         }
     };
 
+    const handleSignInWitFacebook = async () => {
+        try {
+            const result = await signInWithFacebook();
+            dispatch(login(result));
+            toast.success(`Welcome, ${result.displayName}!`);
+        } catch (error) {
+            toast.error("Something went wrong, try again!");
+        }
+    };
+
+
     // Redirect to Profile Page if authenticated
     if (isAuthenticated) {
         return <Navigate to="/profile" replace={true} />;
@@ -48,6 +59,7 @@ const LoginPage: React.FC = () => {
             <div className="p-20 w-3xl flex flex-col justify-center">
                 <h1 className="title">Welcome, Space Traveler!</h1>
                 <p className="subtitle">Please Sign In To Continue</p>
+                {/* Local Email Login Form */}
                 <form onSubmit={handleSubmit} className="my-5">
                     <label>
                         Username:
@@ -67,24 +79,26 @@ const LoginPage: React.FC = () => {
                             onChange={e => setPassword(e.target.value)}
                             className="mt-1"
                             placeholder="Password"
-
                         />
                     </label>
                     <button type="submit" className="btn-primary mt-5 w-full">Login</button>
                 </form>
 
-                 {/* Google Login */}
+                {/* Google Login */}
                 <div>
                     <p className="subtitle mb-4">- Or Sign In With</p>
                     <div className="flex gap-3">
+                        {/* Google Login */}
                         <button className="btn-tertiary flex gap-2 items-center" onClick={handleSignInWithGoogle}>
                             <RiGoogleFill className="text-2xl text-red-600" /> Google
-                            </button>
-                        {/* <button className="btn-tertiary" onClick={handleSubmit}>Facebook</button> */}
+                        </button>
+
+                        {/* Facebook Login */}
+                        <button className="btn-tertiary flex gap-2 items-center" onClick={handleSignInWitFacebook}>
+                            <RiFacebookFill className="text-2xl text-blue-600" /> Facebook
+                        </button>
                     </div>
                 </div>
-
-
             </div>
         </div>
     );
--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,30 +1,51 @@
+// src/services/auth.ts
 import { auth } from '../firebaseConfig'
-import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
+import {
+    FacebookAuthProvider,
+    GoogleAuthProvider,
+    signInWithPopup,
+    signOut
+} from 'firebase/auth'
 
-export const signInWithGoogle = async () => {
-  const provider = new GoogleAuthProvider()
-  const result = await signInWithPopup(auth, provider)
-  const { user } = result
-  if (user) {
-    const { displayName, email, photoURL } = user
-    return { displayName, email, photoURL }
-  }
+// Facebook
+export const signInWithFacebook = async () => {
+    const provider = new FacebookAuthProvider()
+    const result = await signInWithPopup(auth, provider)
+    const { user } = result
+    if (user) {
+        const { displayName, email, photoURL } = user
+        return { displayName, email, photoURL }
+    }
 
-  throw new Error('No user found after authentication.')
+    throw new Error('No user found after authentication.')
 }
 
-export const signInWithEmail = async (email: string, password: string) => {
-  if (email === 'monkey' && password === 'banana') {
-    return {
-      displayName: 'Monkey',
-      email: 'monkey@banana.com',
-      photoURL: 'https://placekitten.com/200/300'
+// Google
+export const signInWithGoogle = async () => {
+    const provider = new GoogleAuthProvider()
+    const result = await signInWithPopup(auth, provider)
+    const { user } = result
+    if (user) {
+        const { displayName, email, photoURL } = user
+        return { displayName, email, photoURL }
     }
-  }
 
-  throw new Error('Invalid email or password.')
+    throw new Error('No user found after authentication.')
+}
+
+// Hard-coded Local Email
+export const signInWithEmail = async (email: string, password: string) => {
+    if (email === 'monkey' && password === 'banana') {
+        return {
+            displayName: 'Monkey',
+            email: 'monkey@banana.com',
+            photoURL: 'https://placekitten.com/200/300'
+        }
+    }
+
+    throw new Error('Invalid email or password.')
 }
 
 export const firebaseSignOut = () => {
-  return signOut(auth)
+    return signOut(auth)
 }
