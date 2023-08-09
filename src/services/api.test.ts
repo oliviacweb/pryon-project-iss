1417fbe355b65822926f6e14d494a3e2fe774e80
Olivia Webster <oliviacweb@gmail.com>
2023-08-09 00:30:08 -0600

    add api call tests for isslocation fetch

---
+++ src/services/api.test.ts
@@ -0,0 +1,36 @@
+import { fetchISSLocation } from './api'
+
+// Mock global.fetch
+;(global.fetch as jest.Mock) = jest.fn()
+
+describe('ISS Location Service', () => {
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('fetches ISS location successfully', async () => {
+    const mockData = {
+      iss_position: {
+        latitude: 123,
+        longitude: 456
+      }
+    }
+
+    ;(global.fetch as jest.Mock).mockResolvedValueOnce({
+      json: jest.fn().mockResolvedValueOnce(mockData)
+    })
+
+    const result = await fetchISSLocation()
+
+    expect(global.fetch).toHaveBeenCalledWith(
+      'http://api.open-notify.org/iss-now.json'
+    )
+    expect(result).toEqual(mockData.iss_position)
+  })
+
+  it('throws an error when fetching fails', async () => {
+    const errorMessage = 'Failed to fetch'
+    ;(global.fetch as jest.Mock).mockRejectedValueOnce(new Error(errorMessage))
+    await expect(fetchISSLocation()).rejects.toThrow(errorMessage)
+  })
+})
