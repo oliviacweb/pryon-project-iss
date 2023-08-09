01d372e039d7ffa7a8af1d678f6c649df90c6e13
Olivia Webster <oliviacweb@gmail.com>
2023-08-08 20:20:24 -0600

    move logout button to sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { useDispatch } from 'react-redux';
+import { logout } from '../redux/slices/authSlice';
 import styles from '../styles/components/SideBar.module.scss';
 
 type Props = {};
 
 export default function SideBar({ }: Props) {
+    const dispatch = useDispatch();
+
+    const handleLogout = () => {
+        dispatch(logout());
+    };
     return (
         <div className={styles.sidebar}>
-            <ul>
-                <li className={styles.link}>
-                    <Link to="/profile"  >Profile</Link>
-
-                </li>
-            </ul>
             {/* Links */}
+            <Link to="/profile" className={styles.link}>Profile</Link>
             <Link to="/astronauts" className={styles.link}>Astronauts</Link>
             <Link to="/iss-location" className={styles.link}>ISS Location</Link>
+            {/* Logout */}
+            <div role="button" onClick={handleLogout} className={styles.link}>
+                Logout
+            </div >
         </div>
     );
 }
--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,23 +1,12 @@
 import React from 'react';
-// Logout
-import { useDispatch } from 'react-redux';
-import { logout } from '../redux/slices/authSlice';
 import Layout from "../components/Layout";
 
 const ProfilePage: React.FC = () => {
-    const dispatch = useDispatch();
-
-    const handleLogout = () => {
-        dispatch(logout());
-    };
 
     return (
         <Layout>
             {/* TODO: Add Username */}
             <h2 className="mb-4">Hi there, "Username"!</h2>
-            <button onClick={handleLogout} className="btn-primary">
-                Logout
-            </button>
         </Layout>
     );
 };
