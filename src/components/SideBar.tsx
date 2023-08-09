a6b268b49c06bbd296fdd75e31079f467c92e157
Olivia Webster <oliviacweb@gmail.com>
2023-08-08 21:19:24 -0600

    update sidebar links and styling

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { logout } from '../redux/slices/authSlice';
 import { firebaseSignOut } from '../services/auth';
+import {
+    RiUserSharedFill,
+    RiSpaceShipFill,
+    RiRadarFill,
+    RiLogoutBoxFill
+} from 'react-icons/ri';
 import styles from '../styles/components/SideBar.module.scss';
 
+const links = [
+    {
+        to: '/profile',
+        icon: <RiUserSharedFill />,
+        label: 'Profile'
+    },
+    {
+        to: '/astronauts',
+        icon: <RiSpaceShipFill />,
+        label: 'Astronauts'
+    },
+    {
+        to: '/iss-location',
+        icon: <RiRadarFill />,
+        label: 'ISS Location'
+    }
+];
+
 export default function SideBar() {
+    const location = useLocation();
     const dispatch = useDispatch();
 
     const handleLogout = async () => {
@@ -15,12 +40,22 @@ export default function SideBar() {
     return (
         <div className={styles.sidebar}>
             {/* Links */}
-            <Link to="/profile" className={styles.link}>Profile</Link>
-            <Link to="/astronauts" className={styles.link}>Astronauts</Link>
-            <Link to="/iss-location" className={styles.link}>ISS Location</Link>
+            {links.map((link, index) => (
+                <Link
+                    key={index}
+                    to={link.to}
+                    className={`${styles.link}
+                    ${location.pathname.includes(link.to)
+                        && styles.activeLink
+                        }`}
+                >
+                    {link.icon} {link.label}
+                </Link>
+            ))}
+
             {/* Logout */}
             <div role="button" onClick={handleLogout} className={styles.link}>
-                Logout
+                <RiLogoutBoxFill /> Logout
             </div >
         </div>
     );
