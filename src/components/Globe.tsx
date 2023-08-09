475fc5d1f2ecbede5774cefc6ad3dd5c6f828f4f
Olivia Webster <oliviacweb@gmail.com>
2023-08-09 00:16:48 -0600

    move fetch and logic to Globe from ISSLocationPage

--- src/components/Globe.tsx
+++ src/components/Globe.tsx
@@ -1,50 +1,64 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Globe from 'react-globe.gl';
 import earthImage from '../assets/images/earth-blue-marble.jpg';
 import spaceImage from '../assets/images/night-sky.png';
-
+import { toast } from 'react-toastify';
+import Loader from "./Loader";
 
 interface MapMarkerProps {
-    lat: number;
-    lng: number;
+    latitude: number;
+    longitude: number;
 }
 
-const MyGlobe: React.FC<MapMarkerProps> = ({ lat, lng }) => {
+const MyGlobe: React.FC = () => {
+    const [location, setLocation] = React.useState<MapMarkerProps | null>(null);
+
     const [dimensions] = React.useState({
         width: window.innerWidth - 290,
         height: window.innerHeight - 305
     });
 
+    const fetchISSLocation = () => {
+        fetch("http://api.open-notify.org/iss-now.json")
+            .then(response => response.json())
+            .then(data => {
+                setLocation(data.iss_position);
+            }).catch(error => {
+                toast.error(`Error:${error.message}`, {
+                    toastId: "ISSLocationPageError"
+                });
+            });
+    };
 
-    // support rendering markers 
+    useEffect(() => {
+        fetchISSLocation();
+        // Fetch the ISS location every 5 seconds
+        const interval = setInterval(() => {
+            fetchISSLocation();
+        }, 5000);
+        // Clear the interval when the component unmounts
+        return () => clearInterval(interval);
+    }, []);
+
+    if (!location) {
+        return <Loader />;
+    }
+
     return <Globe
         waitForGlobeReady={true}
         width={dimensions.width}
         height={dimensions.height}
         backgroundImageUrl={spaceImage}
         globeImageUrl={earthImage}
+        // Ring marker
         ringsData={[
             {
-                lat: lat,
-                lng: lng,
+                lat: location.latitude,
+                lng: location.longitude,
             }
         ]}
         ringAltitude={0.2}
         ringMaxRadius={4}
-
-        // Point markers
-        pointsData={[
-            {
-                lat: lat,
-                lng: lng,
-                altitude: 0.1,
-                radius: 2
-            }
-        ]}
-        pointColor="color"
-        pointAltitude="altitude"
-        pointRadius="radius"
-
     />;
 };
 
--- src/pages/ISSLocationPage.tsx
+++ src/pages/ISSLocationPage.tsx
@@ -1,41 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Layout from "../components/Layout";
-import { toast } from 'react-toastify';
 import Globe from '../components/Globe';
-import Loader from '../components/Loader';
-
-type ISSPosition = {
-  latitude: number;
-  longitude: number;
-};
-
 
 export default function ISSLocationPage() {
-  const [location, setLocation] = React.useState<ISSPosition | null>(null);
-
-  const fetchISSLocation = () => {
-    fetch("http://api.open-notify.org/iss-now.json")
-      .then(response => response.json())
-      .then(data => {
-        setLocation(data.iss_position);
-      }).catch(error => {
-        toast.error(`Error:${error.message}`, {
-          toastId: "ISSLocationPageError"
-        });
-      });
-  };
-
-  useEffect(() => {
-    fetchISSLocation();
-    // Fetch the ISS location every 5 seconds
-    const interval = setInterval(() => {
-      fetchISSLocation();
-    }, 5000);
-    // Clear the interval when the component unmounts
-    return () => clearInterval(interval);
-  }, []);
-
-
   return (
     <Layout>
       <h1 className="title">ISS Location</h1>
@@ -43,7 +10,7 @@ export default function ISSLocationPage() {
         The current location of the International Space Station
       </p>
       <div className="mt-6">
-        {location ? <Globe lat={location.latitude} lng={location.longitude} /> : <Loader />}
+        <Globe />
       </div>
     </Layout>
   );
