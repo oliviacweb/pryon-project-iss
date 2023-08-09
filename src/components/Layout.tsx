20c64345ea5b63f090bec2d1638d772e869dd196
Olivia Webster <oliviacweb@gmail.com>
2023-08-08 19:47:40 -0600

    create layout component and style

---
+++ src/components/Layout.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../redux/store';
+import { Navigate } from "react-router-dom";
+import styles from '../styles/components/Layout.module.scss';
+import Navbar from "./Navbar";
+
+
+type Props = {
+    children: React.ReactNode;
+};
+
+export default function Layout({ children }: Props) {
+    // Navigate to login page if not authenticated
+    const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
+
+    if (!isAuthenticated) {
+        // react-router-dom
+        // https://reactrouter.com/web/api/Hooks
+        <Navigate to="/dashboard" replace={true} />;
+    }
+
+    return (
+        <div className={styles.main}>
+            <Navbar />
+            {children}
+        </div>
+    );
+}
