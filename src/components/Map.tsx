d57f6c708530acd1024b893dd1d11bbe2dc6df46
Olivia Webster <oliviacweb@gmail.com>
2023-08-08 21:13:42 -0600

    update iss page and show basic map

---
+++ src/components/Map.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+
+const containerStyle = {
+    width: '100%',
+    height: 'calc(100vh - 216px)',
+    marginTop: '16px'
+};
+
+const center = {
+    lat: -3.745,
+    lng: -38.523
+};
+
+interface MapMarkerProps {
+    lat: number;
+    lng: number;
+}
+
+const ISSMap: React.FC<MapMarkerProps> = ({ lat, lng }) => {
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: "AIzaSyBplRnJgnPDSAeZ2-e1vWqjyo7s4CeR0bE"
+    });
+
+    const [map, setMap] = React.useState<MapMarkerProps | null>(null);
+
+    // https://www.npmjs.com/package/@react-google-maps/api
+    const onLoad = React.useCallback((map: any) => {
+        setMap(map);
+    }, []);
+
+    const onUnmount = React.useCallback((map: any) => {
+        setMap(null);
+    }, []);
+
+    return isLoaded ? (
+        <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={2}
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+        >
+            {/* Marker */}
+            <>
+                <Marker
+                    position={{ lat: lat, lng: lng }}
+                /></>
+
+        </GoogleMap>
+    ) : <></>;
+};
+
+export default React.memo(ISSMap);
--- src/pages/ISSLocationPage.tsx
+++ src/pages/ISSLocationPage.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Layout from "../components/Layout";
+import Map from '../components/Map';
 
-type Props = {};
+export default function ISSLocationPage() {
+    const [location, setLocation] = React.useState({
+        latitude: 6.1927,
+        longitude: -57.7714
+    });
+    useEffect(() => {
+        fetch("http://api.open-notify.org/iss-now.json")
+            .then(response => response.json())
+            .then(data => {
+                setLocation(data.iss_position);
+            });
+    }, []);
+    return (
+        <Layout>
+            <h1 className="title">ISS Location</h1>
+            <p className="subtitle">
+                The current location of the International Space Station.
+            </p>
 
-export default function ISSLocationPage({ }: Props) {
-  return (
-    <Layout>ISSLocationPage</Layout>
-  );
+            {/* Display the latitude and longitude of the ISS on a map. */}
+            <Map
+                lat={location.latitude}
+                lng={location.longitude}
+            />
+
+        </Layout>
+    );
 }
